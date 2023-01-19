8044cb2e0fea41f35fa71847e11b490365ae3d1f
Aleksandr <105770305+aleksandr-kirakosyan@users.noreply.github.com>
2023-01-19 21:44:45 +0400

    Add files via upload

---
+++ practise/index.js
@@ -0,0 +1,40 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+/*                      CURSOR                          */
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+  cursor.setAttribute("data-fromTop", cursor.offsetTop - scrollY);
+  // console.log(e)
+});
+window.addEventListener("scroll", () => {
+  const fromTop = cursor.getAttribute("data-fromTop");
+  cursor.style.top = scrollY + parseInt(fromTop) + "px";
+  console.log(scrollY);
+});
+window.addEventListener("click", () => {
+  if (cursor.classList.contains("click")) {
+    cursor.classList.remove("click");
+    void cursor.offsetWidth; // trigger a DOM reflow
+    cursor.classList.add("click");
+  } else {
+    cursor.classList.add("click");
+  }
+});
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
