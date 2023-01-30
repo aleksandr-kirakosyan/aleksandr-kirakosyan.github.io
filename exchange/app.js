3323c9fdfd1616985a9a6aac7aa81c0ee22b7502
Aleksandr <105770305+aleksandr-kirakosyan@users.noreply.github.com>
2023-01-30 15:23:21 +0400

    Update app.js

--- exchange/app.js
+++ exchange/app.js
@@ -25,42 +25,56 @@ document.getElementById("demo").innerHTML = today;
 
 fecthAsyncExch()
 
+
 const obj = fetch('https://cb.am/latest.json.php')
-.then(result => result.json())
-.then((output) => {
-  function createTable(obj) {
-    const entries = Object.entries(obj);
-    const tableArray = [];
+  .then(result => result.json())
+  .then((output) => {
+    function createTable(out) {
+      const entries = Object.entries(out);
+      var cols = [];
+      var k = 0;
+      entries.forEach((currentElement, index) => {
+        if (k == '3') k = 0;
+        if (!cols[k]) cols[k] = [];
+        cols[k][index] = currentElement;
+        k++;
+      });
 
-    for (let i = 0; i < entries.length; i += 17) {
-      tableArray.push(entries.slice(i, i + 17));
-    }
+        // Create the table element
+        const table = document.getElementById("table-container");
+        // Create a row for each group of three elements
+        cols.forEach((col) => {
+          const row = document.createElement("tr");
+          col.forEach((entry) => {
+            // Create a div to wrap each pair of cells
+            const cellPair = document.createElement("div");
+            cellPair.style.display = "inline-block";
+  
+            // Create a cell for each entry
+            const cell1 = document.createElement("th");
+            cell1.innerText = entry[0];
+            cell1.style.backgroundColor='rgb(217, 225, 247)'
+            const cell2 = document.createElement("th");
+            cell2.innerText = entry[1];
+            cell2.style.backgroundColor='rgb(217, 225, 247)'
+  
+            // Append the cells to the div
+            cellPair.appendChild(cell1);
+            cellPair.appendChild(cell2);
+  
+            // Append the div to the row
+            row.appendChild(cellPair);
+          });
+          table.appendChild(row);
+        });
+        // Append the table to the HTML page
+        document.getElementById('table-container').appendChild(table);
+      }
+      createTable(output)
+    });
 
-    return tableArray;
-  }
 
-  const tables = createTable(output);
 
-  const tableContainer = document.getElementById("table-container");
-
-  for (const table of tables) {
-    const tableElement = document.createElement("table");
-
-    for (const [key, value] of table) {
-      const row = document.createElement("tr");
-      const keyCell = document.createElement("td");
-      keyCell.innerText = key;
-      const valueCell = document.createElement("td");
-      valueCell.innerText = value;
-      row.appendChild(keyCell);
-      row.appendChild(valueCell);
-      tableElement.appendChild(row);
-    }
-
-    tableContainer.appendChild(tableElement);
-  }
-})
-.catch(error => console.error(error));
 
     
     
@@ -83,6 +97,8 @@ var y = document.getElementById('fordev')
         }
     }
 
+
+    
 
     
 
